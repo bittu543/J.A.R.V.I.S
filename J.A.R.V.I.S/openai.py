2439619f23d3a289b058f8213cf78a8d51ea8145
Bittu Roy <76532877+bittu543@users.noreply.github.com>
2023-09-14 09:05:28 +0530

    openai.py

--- J.A.R.V.I.S/openai.py
+++ J.A.R.V.I.S/openai.py
@@ -2,7 +2,7 @@ import os
 import openai
 from openai import openai
 
-openai.api_key = os.getenv("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")
+openai.api_key = os.getenv("open ai api key")
 
 response = openai.Completion.create(
   model="text-davinci-003",
